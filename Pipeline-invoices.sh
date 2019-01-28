fbada642456a0819988a757129f12ec458051eca
Fernando Blat <ferblape@gmail.com>
2019-01-28 12:19:49 +0100

    Invoices pipeline

---
+++ Pipeline-invoices.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Some variables already defined in .env
+source .env
+
+GOBIERTO_ETL_UTILS=$BASE_DIR/gobierto-etl-utils
+DIPHU_ETL=$BASE_DIR/gobierto-etl-diphu
+WORKING_DIR=/tmp/diphu
+DIPHU_INE_CODE=21000DD000
+URL=http://gobierto-data.s3.amazonaws.com/diphuelva/
+
+rm -rf $WORKING_DIR
+mkdir $WORKING_DIR
+
+# Extract > Download data sources
+cd $GOBIERTO_ETL_UTILS; ruby operations/download-s3/run.rb "diphuelva/invoices" $WORKING_DIR
+
+# Extract > Check valid CSV
+for file in $WORKING_DIR/*.csv; do
+  cd $GOBIERTO_ETL_UTILS; ruby operations/check-csv/run.rb $file
+done
+
+# Transform > Transform planned budgets data files
+for file in $WORKING_DIR/*.csv; do
+  cd $DIPHU_ETL; ruby operations/gobierto_budgets/transform-invoices/run.rb $file $file"_transformed.json"
+done
+
+# Load > Import invoices
+for file in $WORKING_DIR/*_transformed.json; do
+  cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/import-invoices/run.rb $file
+done
+
+echo $DIPHU_INE_CODE > $WORKING_DIR/organization.id.txt
+
+# Load > Publish activity
+cd $GOBIERTO_DIR; bin/rails runner $GOBIERTO_ETL_UTILS/operations/gobierto/publish-activity/run.rb providers_updated $WORKING_DIR/organization.id.txt
+
+# Clear cache
+cd $GOBIERTO_DIR; bin/rails runner $GOBIERTO_ETL_UTILS/operations/gobierto/clear-cache/run.rb
---
+++ operations/gobierto_budgets/transform-invoices/run.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+
+require "bundler/setup"
+Bundler.require
+
+require "json"
+require "csv"
+
+# Usage:
+#
+#  - Must be ran as an independent Ruby script
+#
+# Arguments:
+#
+#  - 0: Absolute path to a file with the input data
+#  - 1: Absolute path of the output file
+#
+# Samples:
+#
+#   /path/to/project/operations/gobierto_budgets/transform-invoices/run.rb input.csv output.json
+#
+
+if ARGV.length != 2
+  raise "At least one argument is required"
+end
+
+input_file = ARGV[0]
+output_file = ARGV[1]
+
+puts "[START] transform-invoices/run.rb with file=#{input_file} output=#{output_file}"
+
+base_attributes = {
+  location_id: '21000DD000',
+  province_id: nil,
+  autonomous_region_id: nil,
+  payment_date: nil,
+  economic_budget_line_code: nil,
+  functional_budget_line_code: nil
+}
+
+output_data = []
+CSV::Converters[:strip] = lambda { |s| s.try(:strip) }
+CSV.read(input_file, headers: true, encoding: 'utf-8', header_converters: lambda { |h| h.downcase.gsub(' ', '_') }, converters: [:strip]).each do |row|
+
+  next if row["fecha_fac"].blank?
+
+  begin
+    m,d,y = row["fecha_fac"].split('/').map(&:to_i)
+    date = Date.new(y,m,d)
+  rescue
+    puts row
+    exit
+  end
+
+  attributes = {
+    value: row['importe'].to_f,
+    date: date.strftime("%Y-%m-%d"),
+    invoice_id: row['factura'],
+    provider_id: row['tercero'],
+    provider_name: row['nombre_ter.'],
+    paid: true,
+    subject: row['texto_libre'],
+    freelance: row['tercero'] !~ /\A[A-Z]/i
+  }
+
+  output_data << attributes.merge(base_attributes)
+end
+
+File.write(output_file, output_data.to_json)
+
+puts "[END] transform-invoices/run.rb output=#{output_file}"
