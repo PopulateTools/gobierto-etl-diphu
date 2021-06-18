b85b385f61d6888cc860ddedd5f526798610ef1b
stbn <stbnrivas@gmail.com>
2021-06-18 14:00:23 +0200

    delay deletion data until check existence of new

--- Pipeline-budgets.sh
+++ Pipeline-budgets.sh
@@ -14,30 +14,30 @@ MAPPINGS_FILE=$DATA_DIR/custom_categories_mapping_2021.csv
 rm -rf $WORKING_DIR
 mkdir -p $WORKING_DIR/presupuestos
 
-# Create organization id
+# Extract > Create organization id
 echo $DIPHU_INE_CODE > $WORKING_DIR/organization.id.txt
 
-# Clear previous budgets
-cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/clear-budgets/run.rb $WORKING_DIR/organization.id.txt
-
-# Copy data to WORKING_DIR
+# Extract > Copy data to WORKING_DIR
 cp -R $DATA_DIR/* $WORKING_DIR/
 
 # Extract > Extract custom categories
 cd $DIPHU_ETL; ruby $DIPHU_ETL/operations/gobierto_budgets/extract-custom-categories/run.rb $CURRENT_YEAR_CUSTOM_BUDGETS $MAPPINGS_FILE $WORKING_DIR/custom_categories.json
-
-# Load > Import custom categories
-cd $GOBIERTO_DIR; bin/rails runner $DIPHU_ETL/operations/gobierto_budgets/import-custom-categories/run.rb $WORKING_DIR/custom_categories.json $GOBIERTO_DOMAIN
 
 # Transform > Transform budgets data files
 for file in $WORKING_DIR/presupuestos/*; do
   cd $DIPHU_ETL; ruby operations/gobierto_budgets/transform-budgets/run.rb $file $file"_transformed.json" $MAPPINGS_FILE
 done
 
+# Load > Clear previous budgets
+cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/clear-budgets/run.rb $WORKING_DIR/organization.id.txt
+
 # Load > Import transformed data
 for file in $WORKING_DIR/presupuestos/*_transformed.json; do
   cd $DIPHU_ETL; ruby operations/gobierto_budgets/import-budgets/run.rb $file
 done
+
+# Load > Import custom categories
+cd $GOBIERTO_DIR; bin/rails runner $DIPHU_ETL/operations/gobierto_budgets/import-custom-categories/run.rb $WORKING_DIR/custom_categories.json $GOBIERTO_DOMAIN
 
 # Load > Calculate totals
 cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/update_total_budget/run.rb "$YEARS" $WORKING_DIR/organization.id.txt
